5ac282bc7ffdbf4c2a702d25327d0fa2d5dfc060
michael rokitko <alarn777@gmail.com>
2019-05-12 20:43:06 +0300

    Initial react project created Initial guidelines created

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GUI Application for MATAYZE Project.
+We use https://material-ui.com/ as a base for this project
--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <text>Hello people, this is our project</text>
+          <text>Hello people, this is our project, we shall use only <a href="https://material-ui.com/">material-ui.com</a> for this project</text>
+          <text>Before you write a component check if you can find it there.</text>
       </header>
     </div>
   );
