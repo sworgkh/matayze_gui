fc269ee3c8379fdaafe1775b15d9a621baae280e
EranMaron <eran9maron@gmail.com>
2019-05-16 23:42:51 +0300

    Card Component Was Added

--- src/Screens/hall_interface/hall_index.js
+++ src/Screens/hall_interface/hall_index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import Card from './components/Card'
+
+const styles = {
+    pageContainer: {
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: 'linear-gradient(to top left, purple, black)',
+    }
+}
+
 export default class hall_index extends React.Component{
     constructor(props){
         super(props);
     }
     render() {
         return (
-            <div>
-                <h1>hall_index</h1>
+            <div style={styles.pageContainer}>
+                <Card />
+                <Card />
+                <Card />
             </div>
         );
     }
