e14e942e18e0f59aba9d264e71b46e82266ce86d
MarkIoshpa <mark.ioshpa@gmail.com>
2019-05-17 15:24:25 +0300

    Updated website interface

--- src/Screens/site_interface/components/header.js
+++ src/Screens/site_interface/components/header.js
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography'
 const Header = (props) => {
   return (
     <div style={props.style}>
-      <Typography style={{ color: '#FFFFFF'}} variant='h3'>
+      <Typography style={{ color: '#FFFFFF', textAlign: 'center'}} variant='h3'>
         Event Schedule
       </Typography>
     </div>
