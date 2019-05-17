d2b1867dc1299a99dcd74e3084e180d9e681e54a
amit levy <amit.p.levy@gmail.com>
2019-05-17 10:38:15 +0300

    hall screen

---
+++ src/Screens/hall_interface/components/Time.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Clock from "react-live-clock";
+
+const styles = {
+  heading: {
+    color: "white"
+  }
+};
+
+export default class Time extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const date = new Date();
+
+    return (
+      <div style={styles.heading}>
+        <Clock format={"HH:mm"} ticking={true} timezone={"Israel"} /> |{" "}
+        {date.getDay()}/{date.getMonth()}/{date.getFullYear()}
+      </div>
+    );
+  }
+}
--- src/Screens/hall_interface/hall_index.js
+++ src/Screens/hall_interface/hall_index.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import Card from './components/Card'
+import Card from "./components/Card";
+import Time from "./components/Time";
 
 const styles = {
-    pageContainer: {
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: 'linear-gradient(to top left, purple, black)',
-    }
-}
+  pageContainer: {
+    width: "100vw",
+    height: "100vh",
+    backgroundImage: "linear-gradient(to top left, purple, black)"
+  },
+  heading: {
+    color: "white"
+  }
+};
 
-export default class hall_index extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render() {
-        return (
-            <div style={styles.pageContainer}>
-                <Card />
-                <Card />
-                <Card />
-            </div>
-        );
-    }
+export default class hall_index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div style={styles.pageContainer}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <h1 style={styles.heading}>Confrance Name</h1>
+          <div
+            style={{
+              width: "100px",
+              height: "100px",
+              backgroundColor: "white"
+            }}
+          />
+          <Time />
+        </div>
+
+        <hr />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
+  }
 }
