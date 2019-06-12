7748a821ecb2086e2db4090f723771b7a7df2750
michael rokitko <alarn777@gmail.com>
2019-06-12 18:44:24 +0300

    Fixed fetch hall_index

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ class App extends React.Component {
 
     this.setState({userData:userData})
     console.log(userData.username)
-    this.halls()
+    // this.halls()
     // return;
     if(userData.username.toString() === 'Micahel' || userData.username.toString() === 'Admin'){
         console.log('here')
